c496a2f1f84ce70791ee22dffff68bc402c08a85
AveYo <aveyo@yahoo.com>
2025-04-14 20:01:15 +0300

    reliability improvements
    added an extra top variable to toggle $ShowGameIcons (1 = enable)  
    
    revised launch options to -- steamwebhelper crapping itself
    
    revised desktop shortcut to -- windows 11 headless quirks

--- steam_min.bat
+++ steam_min.bat
@@ -1,16 +1,23 @@
-@(set ^ "0=%~f0" -des ') & powershell -nop -c iex(out-string -i (gc -lit $env:0)) & exit /b ');.{
+<# ::
+@echo off & set "0=%~f0" & powershell -nop -c iex(out-string -i (gc -lit $env:0)) & exit /b
+#>.{
 
-" Steam_min : always starts in SmallMode with reduced ram usage when idle - AveYo, 2025.03.10 " 
+" Steam_min : always starts in SmallMode with reduced ram usage when idle - AveYo, 2025.04.14 " 
+
+$FriendsSignIn = 0
+$ShowGameIcons = 0
 
 $QUICK = '-silent -quicklogin -vgui -oldtraymenu -nofriendsui -no-dwrite -vrdisable -forceservice -console ' + 
-         '-cef-force-browser-underlay -cef-delaypageload -cef-force-occlusion -cef-disable-gpu -cef-single-process'
+         '-cef-force-browser-underlay -cef-delaypageload -cef-force-occlusion ' +
+         '-cef-single-process -cef-in-process-gpu -cef-disable-gpu-compositing -cef-disable-gpu' 
 
 $STEAM = resolve-path (gp "HKCU:\SOFTWARE\Valve\Steam" -ea 0).SteamPath
-$LASTW = 20250310
+$stamp = 20250414
 
-$SignIntoFriends = 0
+##  AveYo: minimize script
+powershell -win 2 -nop -c ';' 
 
-#:: AveYo: lean and mean helper functions to process steam vdf files
+##  AveYo: lean and mean helper functions to process steam vdf files
 function vdf_read {
   param([string[]]$vdf, [ref]$line=([ref]0), [string]$r='\A\s*("(?<k>[^"]+)"|(?<b>[\{\}]))\s*(?<v>"(?:\\"|[^"])*")?\Z') #
   $obj = new-object System.Collections.Specialized.OrderedDictionary # ps 3.0: [ordered]@{}
@@ -50,7 +57,7 @@ function sc-nonew($fn,$txt) {
   else { [IO.File]::WriteAllText($fn, $txt -join "`n") } # ps2.0
 }
 
-#_# AveYo: change steam startup location to Library window
+##  AveYo: change steam startup location to Library window
 dir "$STEAM\userdata\*\7\remote\sharedconfig.vdf" -Recurse |foreach {
   $file = $_; $write = $false; $vdf = vdf_read -vdf (gc $file -force)
   vdf_mkdir $vdf["SteamDefaultDialog"] 'Software\Valve\Steam'
@@ -59,8 +66,9 @@ dir "$STEAM\userdata\*\7\remote\sharedconfig.vdf" -Recurse |foreach {
   if ($write) { sc-nonew $file $(vdf_write $vdf); write-output " $file " }
 }
 
-#_# AveYo: enable Small Mode and library performance options
-$opt = @{LibraryDisableCommunityContent=1;LibraryLowBandwidthMode=1;LibraryLowPerfMode=1;LibraryDisplayIconInGameList=0}
+##  AveYo: enable Small Mode and library performance options
+$opt = @{LibraryDisableCommunityContent=1; LibraryLowBandwidthMode=1; LibraryLowPerfMode=1; LibraryDisplayIconInGameList=0}
+if ($ShowGameIcons -eq 1) {$opt.LibraryDisplayIconInGameList = 1}
 dir "$STEAM\userdata\*\config\localconfig.vdf" -Recurse |foreach {
   $file = $_; $write = $false; $vdf = vdf_read -vdf (gc $file -force)
   vdf_mkdir $vdf["UserLocalConfigStore"] 'Software\Valve\Steam'
@@ -69,7 +77,7 @@ dir "$STEAM\userdata\*\config\localconfig.vdf" -Recurse |foreach {
   foreach ($o in $opt.Keys) { if ($vdf["UserLocalConfigStore"]["$o"] -ne "`"$($opt[$o])`"") {
     $vdf["UserLocalConfigStore"]["$o"] = "`"$($opt[$o])`""; $write = $true
   }}
-  if ($SignIntoFriends -eq 0) {
+  if ($FriendsSignIn -eq 0) {
     vdf_mkdir $vdf["UserLocalConfigStore"] 'friends'
     $key = $vdf["UserLocalConfigStore"]["friends"]
     if ($key["SignIntoFriends"] -ne '"0"') { $key["SignIntoFriends"] = '"0"'; $write = $true }
@@ -77,7 +85,7 @@ dir "$STEAM\userdata\*\config\localconfig.vdf" -Recurse |foreach {
   if ($write) { sc-nonew $file $(vdf_write $vdf); write-output " $file " }
 }
 
-#_# AveYo: add steam_reset.bat
+##  AveYo: add steam_reset.bat
 if (-not (test-path "$STEAM\steam_reset.bat")) { set-content "$STEAM\steam_reset.bat" @'
 @reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Steam /f
 @reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\steamwebhelper.exe" /f
@@ -88,24 +96,24 @@ if (-not (test-path "$STEAM\steam_reset.bat")) { set-content "$STEAM\steam_reset
 '@ -force
 }
 
-#_# AveYo: was this directly pasted into powershell? then we must save on disk
-if (!$env:0 -or $env:0 -ne "$STEAM\steam_min.bat" -or $LASTW -lt 20250310) {
-  $0 = @('@(set ^ "0=%~f0" -des '') & powershell -nop -c iex(out-string -i (gc -lit $env:0)) & exit /b '');.{' +  
-  ($MyInvocation.MyCommand.Definition) + '};$_press_Enter_if_pasted_in_powershell') -split'\r?\n'
-  set-content "$STEAM\steam_min.bat" $0 -force
+##  AveYo: was this directly pasted into powershell? then we must save on disk
+if (!$env:0 -or $env:0 -ne "$STEAM\steam_min.bat" -or $stamp -lt 20250414) {
+  $f0 = @("<# ::`n"+'@echo off & set "0=%~f0" & powershell -nop -c iex(out-string -i (gc -lit $env:0)) & exit /b'+
+          "`n#>.{"+($MyInvocation.MyCommand.Definition)+'};$_press_Enter_if_pasted_in_powershell') -split'\r?\n'
+  set-content "$STEAM\steam_min.bat" $f0 -force
 } 
 
-#_# AveYo: refresh Steam_min desktop shortcut and startup run if enabled
+##  AveYo: refresh Steam_min desktop shortcut and startup run if enabled
 $short = "$([Environment]::GetFolderPath('Desktop'))\Steam_min.lnk"
 $s = (new-object -ComObject WScript.Shell).CreateShortcut($short)
-if (-not (test-path $short) -or $s.Arguments -notmatch 'steam_min') {
-  $s.Description = "$STEAM\steam.exe"; $s.IconLocation = "$STEAM\steam.exe,0" 
-  $s.TargetPath = "conhost"; $s.Arguments = "--headless `"$STEAM\steam_min.bat`""; $s.Save()
+if (-not (test-path $short) -or $s.Arguments -notmatch "$stamp") {
+  $s.Description = "$STEAM\steam.exe"; $s.IconLocation = "$STEAM\steam.exe,0"; $s.WindowStyle = 7 # min
+  $s.TargetPath = "$STEAM\steam_min.bat"; $s.Arguments = "$stamp"; $s.Save()
 }
 $start = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
-if (gp $start Steam -ea 0) { sp $start Steam "conhost --headless `"$STEAM\steam_min.bat`"" }
+if (gp $start Steam -ea 0) { sp $start Steam "$STEAM\steam_min.bat" }
 
-#_# AveYo: start Steam with quick launch options - now under explorer parent
+##  AveYo: start Steam with quick launch options - now under explorer parent
 ni "HKCU:\Software\Classes\.steam_min\shell\open\command" -force >''
 sp "HKCU:\Software\Classes\.steam_min\shell\open\command" "(Default)" "`"$STEAM\steam.exe`" $QUICK"
 if (!(test-path "$STEAM\.steam_min")) { set-content "$STEAM\.steam_min" "" }
